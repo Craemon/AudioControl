4a610c04c89c8eddba71b86f9a6722d36b85130a
Craemon <121398546+Craemon@users.noreply.github.com>
2026-01-22 23:31:53 +0100

    feat: read out sliders as string

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{BufRead, BufReader};
+use std::time::Duration;
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("Starting Audio Control!");
+    let port_name = "/dev/ttyUSB0";
+    let baud_rate:u32 = 9600;
+
+    let port = serialport::new(port_name, baud_rate)
+        .timeout(Duration::from_millis(1000))
+        .open()?;
+
+    let mut reader = BufReader::new(port);
+
+    loop {
+        let mut line = String::new();
+        if reader.read_line(&mut line)? > 0 {
+            println!("Received: {}", line.trim());
+        }
+    }
 }
